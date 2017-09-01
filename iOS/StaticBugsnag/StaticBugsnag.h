142b9124403f66bb02226eaf3200db79b21c13f1
Kevin Barnes <kevin.barnes@shopify.com>
2017-09-01 10:09:08 -0400

    I had a dream

---
+++ iOS/StaticBugsnag/StaticBugsnag.h
@@ -0,0 +1,15 @@
+//
+//  StaticBugsnag.h
+//  StaticBugsnag
+//
+//  Created by Kevin Barnes on 2017-09-01.
+//  Copyright © 2017 Bugsnag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "Bugsnag.h"
+
+@interface StaticBugsnag : NSObject
+
+@end
---
+++ iOS/StaticBugsnag/StaticBugsnag.m
@@ -0,0 +1,13 @@
+//
+//  StaticBugsnag.m
+//  StaticBugsnag
+//
+//  Created by Kevin Barnes on 2017-09-01.
+//  Copyright © 2017 Bugsnag. All rights reserved.
+//
+
+#import "StaticBugsnag.h"
+
+@implementation StaticBugsnag
+
+@end
